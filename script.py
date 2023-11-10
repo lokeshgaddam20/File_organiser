84a99b5429a42b18e5d126706f8903ce3b5187b7
Lokesh Gaddam <gaddamlokesh20@gmail.com>
2023-11-10 19:49:31 +0530

    final commit

--- script.py
+++ script.py
@@ -7,28 +7,34 @@ import pathlib
 def organize_files(mypath):
     
     files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-    file_type_variation_list=[]
-    filetype_folder_dict={}
+    filetype_list=[]
+    filetype_path={}
     
+    # create list,dict for each filetype
     for file in files:
-        filetype=pathlib.Path(file).suffix
+        # get file extension
+        filetype=pathlib.Path(file).suffix 
         
-        if filetype not in file_type_variation_list:
-            file_type_variation_list.append(filetype)
+        #filetype not in list
+        if filetype not in filetype_list:
+            filetype_list.append(filetype)
             new_folder_name=mypath+'/'+ filetype + '_folder'
-            filetype_folder_dict[str(filetype)]=str(new_folder_name)
+            filetype_path[str(filetype)]=str(new_folder_name)
             
-            if os.path.isdir(new_folder_name)==True: #folder exists
+            #folder exists
+            if os.path.isdir(new_folder_name)==True: 
                 continue
             else:
                 os.mkdir(new_folder_name)
-    
-        print(filetype_folder_dict)
+                
+    # move files to respective folders
     for file in files:
         src_path=mypath+'/'+file
         filetype=pathlib.Path(file).suffix
-        if filetype in filetype_folder_dict.keys():
-            dest_path=filetype_folder_dict[str(filetype)]
+        
+        #filetype in dict
+        if filetype in filetype_path.keys():
+            dest_path=filetype_path[str(filetype)]
             shutil.move(src_path,dest_path)
             print(src_path + '>>>' + dest_path)
 if __name__=="__main__":
