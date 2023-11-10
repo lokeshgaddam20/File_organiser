7836f7ebfea4c1d9e06f038d6848af28816843d5
Lokesh Gaddam <gaddamlokesh20@gmail.com>
2023-11-10 19:24:36 +0530

    test commit

--- script.py
+++ script.py
@@ -8,25 +8,30 @@ def sort_files_in_a_folder(mypath):
     A function to sort the files in a download folder
     into their respective categories
     '''
-    print(stated)
+    print("stated")
     files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
     file_type_variation_list=[]
     filetype_folder_dict={}
+    print("Before")
     for file in files:
         print(file)
         filetype=pathlib.Path(file).suffix
         if filetype not in file_type_variation_list:
+            print("if")
             file_type_variation_list.append(filetype)
             new_folder_name=mypath+'/'+ filetype + '_folder'
             filetype_folder_dict[str(filetype)]=str(new_folder_name)
-            if os.path.isdir(new_folder_name)==True:  #folder exists
+            if os.path.isdir(new_folder_name)==True: 
+                print("Exist")#folder exists
                 continue
             else:
                 os.mkdir(new_folder_name)
+    print("After")
     for file in files:
         src_path=mypath+'/'+file
         filetype=file.split('.')[1]
         if filetype in filetype_folder_dict.keys():
+            print("ifs")
             dest_path=filetype_folder_dict[str(filetype)]
             shutil.move(src_path,dest_path)
         print(src_path + '>>>' + dest_path)
